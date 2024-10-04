27bc98dc34ea4cd89f8fccdde08aa1d1fda65d59
DempseyTran <25A4041500@hvnh.edu.vn>
2024-10-04 10:10:05 +0700

    4-10 Add Js, some css for Slides show HomePage

---
+++ assets/src/Homepage_Section1_Slides.js
@@ -0,0 +1,39 @@
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll('.slides img');
+const dots = document.querySelectorAll('.nav-dot');
+
+// hàm show currentSlide
+function showSlide(index) {
+    // nếu slide hiện tại là 3 chẳng hạn, thì slide 3 chính là display: block, còn 1,2,4 là none (dấu ảnh)
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+
+    // update dots, khi một slide mới được hiển thị (chỉ số của slide là index), các chấm tròn sẽ được cập nhật để chỉ rõ slide hiện tại.
+    dots.forEach((dot, i) => {
+        dot.className = i === index ? 'nav-dot active' : 'nav-dot';
+    });
+
+    currentSlideIndex = index;
+}
+
+// hàm chuyển ảnh
+function autoSlide() {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
+}
+// set vòng lặp 3s
+let slideInterval = setInterval(autoSlide, 3000);
+
+// Thanh điều hướng các dots
+function currentSlide(index) {
+    // xóa lặp auto chuyển slides trước khi set vòng lặp mới
+    clearInterval(slideInterval); 
+    showSlide(index - 1);
+
+    // restart lại vòng lặp
+    slideInterval = setInterval(autoSlide, 3000); 
+}
+
+// bắt đầu show từ currentSlide
+showSlide(currentSlideIndex);
